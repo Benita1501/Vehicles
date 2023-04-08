b47a210610e057f7d131a130d6e06dcec4de229a
Benita1501 <benichris94@gmail.com>
2023-04-08 12:35:15 +0530

    Travelscodemaster

---
+++ src/org/vehicle/Vehicles.java
@@ -0,0 +1,15 @@
+package org.vehicle;
+
+public class Vehicles {
+
+	private void car() {
+		// TODO Auto-generated method stub
+
+		System.out.println("car");
+	}
+	
+	public static void main(String[] args) {
+		Vehicles v =  new Vehicles();
+		v.car();
+	}
+}
